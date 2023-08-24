b1673598a369bae6bb9ee4923a096167f8a3c2ec
Davi Feliciano <dfeliciano37@gmail.com>
2023-08-24 20:44:47 -0300

    fix: disable timestamp conversions from node-pg

--- src/database/pool.js
+++ src/database/pool.js
@@ -5,6 +5,11 @@ dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
 
+pg.types.setTypeParser(
+  pg.types.builtins.TIMESTAMP,
+  (stringValue) => stringValue
+);
+
 const pool = new pg.Pool({
   connectionString,
   ssl: process.env.MODE === "production",
