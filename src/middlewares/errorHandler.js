f46e100377051534a16770c9605ca3bd7430ee0b
Davi Feliciano <dfeliciano37@gmail.com>
2023-08-31 20:17:55 -0300

    fix: wrong parameter order in error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 import httpStatus from "http-status";
 import HttpError from "../errors/common/HttpError.js";
 
-export default function errorHandler(req, res, next, err) {
+export default function errorHandler(err, req, res, next) {
   if (err instanceof HttpError) {
     return res.status(err.statusCode).send({ message: err.message });
   }
